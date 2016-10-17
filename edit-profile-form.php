e17e0072fd0778f5ccb73d32866e4f821d31bb63
MatHatter <quiksilver67213@yahoo.com>
2016-10-17 18:42:56 -0400

    Add files via upload
    edited the </div> tags.

---
+++ edit-profile-form.php
@@ -0,0 +1,147 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+<form action="../../Templates/components/update-profile.php" method="post" enctype="multipart/form-data" id="UploadForm">
+    <!-- Nav tabs -->  
+    <ul id="myTab" class="nav nav-tabs">
+      <li class="active"><a href="#general" data-toggle="tab">General</a></li>
+      <li><a href="#personal" data-toggle="tab">Personal</a></li>
+    </ul>
+    <!-- Tab panes -->
+    <div id="content" class="tab-content">
+        <div class="tab-pane fade in active" id="general">         
+            <div class="col-md-6">
+                <div class="form-group float-label-control">                      
+                    <label for="float-input-1">First Name</label>
+                    <input id="float-input-1" type="text" class="form-control" placeholder="<?php echo $rws['user_firstname'];?>" name="user_firstname" value="<?php echo $rws['user_firstname'];?>">
+                </div>
+
+                <div class="form-group float-label-control">  
+                    <label for="">Last Name</label>
+                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_lastname'];?>" name="user_lastname" value="<?php echo $rws['user_lastname'];?>">
+                </div>
+  
+                <div class="form-group float-label-control">
+                    <label for="">Avatar</label>
+                    <input name="ImageFile" type="file" id="uploadFile"/>
+                    
+                    <div class="col-md-6">
+                        <div class="shortpreview">
+                        	<div class="form-group float-label-control">
+                            <label for="">Previous Avatar</label>
+                            <br> 
+                            <img src="userfiles/avatars/<?php echo $rws['user_avatar'];?>" class="img-responsive">
+                            </div>
+                        </div> <!--close Short preview for Previous Avatar-->
+                    </div> <!-- close "col-md-6" for Previous Avatar-->
+                 </div>    <!-- close "form-group float-label-control"-->
+                    
+                        <div class="shortpreview" id="uploadImagePreview">
+                            <label for="">Current Uploaded Avatar </label>
+                            <br> 
+                            <div id="imagePreview">
+                            </div>
+                        </div>   
+       </div>  <!--closing the "col-md-6" for the general tab. -->
+        
+              <div class="col-md-6">
+          		<div class="form-group float-label-control">
+                <label for="">Username</label>
+                <input type="text" class="form-control" placeholder="<?php echo $rws['user_username'];?>" name="user_username" value="<?php echo $rws['user_username'];?>">
+                
+                </div>
+                <div class="form-group float-label-control">
+                        <!--<div class="input-group">-->
+                    <label for="">Password</label>
+                    <input type="password" class="form-control" placeholder="<?php echo $rws['user_password'];?>" name="user_password" value="<?php echo $rws['user_password'];?>">
+                </div>
+                <div class="form-group float-label-control">
+                    <label for="">Email</label> 
+                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_email'];?>" name="user_email" value="<?php echo $rws['user_email'];?>">
+                </div> 
+                
+                
+                <br>
+    <div class="submit">
+        <center>
+            <button class="btn btn-primary ladda-button" data-style="zoom-in" type="submit"  id="SubmitButton" value="Upload" />Save Your Profile</button>
+        </center>
+    </div>     
+               </div> <!-- closing the "col-md-6" for the Username, Password, and Email."-->
+           </div> <!--closing the General Tab-->
+
+        <div class="tab-pane fade" id="personal">
+            <div class="col-md-6">
+                <div class="form-group float-label-control">
+                    <label for="">Short Bio</label>
+                    <textarea class="form-control" placeholder="<?php echo $rws['user_shortbio'];?>" rows="10" placeholder="<?php echo $rws['user_shortbio'];?>" name="user_shortbio" value="<?php echo $rws['user_shortbio'];?>"><?php echo $rws['user_shortbio'];?></textarea>
+                </div>
+                <div class="form-group float-label-control">
+                    <label for="">Birthday</label>   
+                    <input type="date" class="form-control" placeholder="<?php echo $rws['user_dob'];?>" name="user_dob" value="<?php echo $rws['user_dob'];?>">      
+                </div>
+                <div class="form-group float-label-control">
+                    <label for="">Profession</label>
+                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_profession'];?>" name="user_profession" value="<?php echo $rws['user_profession'];?>" id="profession">    
+                		
+                </div>  
+                <label for="">Gender</label>              
+                <div class="form-group float-label-control">
+                    <div class="radio-inline">
+                        <label>
+                            <input type="radio" name="user_gender" id="optionsRadios1" value="Male" checked>Male</label>
+                    </div>              
+                    <div class="radio-inline">
+                        <label>
+                            <input type="radio" name="user_gender" id="optionsRadios1" value="Female">Female</label>
+                    </div>
+                </div>
+                <div class="form-group float-label-control">
+                    <label for="">Country</label>
+                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_country'];?>" name="user_country" value="<?php echo $rws['user_country'];?>" id="country">    
+                </div>
+           
+                <div class="form-group float-label-control">
+                    <label for="">Address</label>
+                    <input type="text" class="form-control" placeholder="<?php echo $rws['user_address'];?>" name="user_address" value="<?php echo $rws['user_address'];?>">    
+                </div>
+                <label for="">Website</label>
+                <div class="form-group float-label-control">
+                    <div class="input-group">                  
+                        <span class="input-group-addon">http://</span>
+                        <input type="text" class="form-control" placeholder="<?php echo $rws['user_website'];?>" name="user_website" value="<?php echo $rws['user_website'];?>">                  		
+                    </div>
+                </div>
+                <br>
+    <div class="submit">
+        <center>
+            <button class="btn btn-primary ladda-button" data-style="zoom-in" type="submit"  id="SubmitButton" value="Upload" />Save Your Profile</button>
+        </center>
+    </div>
+            </div> <!-- close "col-md-6" for personal page-->
+         </div> <!-- close personal tab-->
+      </div> <!-- close tab form. -->
+           
+           
+
+</form>
+
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+
+
+
+</head>
+<body>
+</body>
+</html>
